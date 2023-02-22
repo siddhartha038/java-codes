f623ee1b3cfa6ca07289cad1db53213e437d3c2f
Siddhartha Singh <siddhartha038>
2023-02-22 20:51:48 +0530

    add and commit

---
+++ dowhile.java
@@ -0,0 +1,9 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a < 10);
+    }
+}
