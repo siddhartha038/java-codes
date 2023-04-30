068e8859446c4c3017a50409fd36cf72a90ea8a0
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2023-04-30 18:41:21 +0530

    add via upload

---
+++ Train.java
@@ -0,0 +1,19 @@
+class Train {
+    // public int reservation;
+    // public int age;
+    public int id, seat;
+
+    Train() {
+        this.id = 123;
+        this.seat = 100;
+    }
+
+    public Train(int id, int seat) {
+        this.id = id;
+        this.seat = seat;
+    }
+
+    public void display() {
+        System.out.println("TRAIN ID-" + id + "\n" + "SEATS-" + seat);
+    }
+}
