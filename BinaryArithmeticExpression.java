83ccb0764ad570a2953ad2628871820fd5b0e997
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2023-05-19 10:48:36 +0530

    All files via upload

---
+++ BinaryArithmeticExpression.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BinaryArithmeticExpression {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a binary arithmetic expression: ");
+        String expression = scanner.nextLine();
+
+        String[] tokens = expression.split(" ");
+        if (tokens.length != 3) {
+            System.out.println("Invalid expression!");
+            return;
+        }
+
+        int operand1 = Integer.parseInt(tokens[0], 2);
+        int operand2 = Integer.parseInt(tokens[2], 2);
+        String operator = tokens[1];
+
+        int result;
+
+        switch (operator) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                result = operand1 / operand2;
+                break;
+            case "%":
+                result = operand1 % operand2;
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        String binaryResult = Integer.toBinaryString(result);
+        System.out.println("Result: " + binaryResult);
+    }
+}
+
