4e79ee65195f3c189d2c174ea4673be7a11f6f69
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-01-30 18:07:12 +0530

    Add files via upload

---
+++ complexno.java
@@ -0,0 +1,11 @@
+class ComplexAddition {
+    public static void main(String[] args) {
+        double real1 = 3.5, imag1 = 2.5;
+        double real2 = 1.5, imag2 = 4.5;
+
+        double sumReal = real1 + real2;
+        double sumImag = imag1 + imag2;
+
+        System.out.println("Sum: " + sumReal + " + " + sumImag + "i");
+    }
+}
